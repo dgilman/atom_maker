98bb7794b9a0b0ab310339e13029736d908c9c73
David Gilman <davidgilman1@gmail.com>
2011-11-13 11:17:14 -0600

    GPLv3!

--- am.py
+++ am.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 # vim: ts=3 et sw=3 sts=3:
+
+# Copyright 2011 David Gilman
+#
+# This file is part of atom_maker.
+#
+# atom_maker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# atom_maker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with atom_maker.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys, os
 
 VERSION = 1
--- generators.py
+++ generators.py
@@ -1,5 +1,21 @@
 # vim: ts=3 et sw=3 sts=3:
 
+# Copyright 2011 David Gilman
+#
+# This file is part of atom_maker.
+#
+# atom_maker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# atom_maker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 19 # along with atom_maker.  If not, see <http://www.gnu.org/licenses/>.
+
 #when adding a generator don't forget to add it to the dict at the end of the file!
 
 #generator spec:
