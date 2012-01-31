ec2c06eb98f341682d52d1f8f6e9c3e0f5be2477
David Gilman <davidgilman1@gmail.com>
2012-01-31 10:17:00 -0600

    Make err() more noisy
    create_error_feed()'s IDs used to be constant across all feeds and
    errors.  A timestamp is now appended to the ID so a conforming RSS
    reader will show a new entry each time a fetch results in an error.
    
    There's also an optional silent arg to err() which suppresses output.

--- util.py
+++ util.py
@@ -5,22 +5,30 @@ noarg = "This generator has a mandatory primary argument (arg).  You need to inc
 def rfc3339(d): # https://bitbucket.org/henry/rfc3339/src/tip/rfc3339.py 
     return ('%04d-%02d-%02dT%02d:%02d:%02dZ' % (d.year, d.month, d.day, d.hour, d.minute, d.second))
 
-def create_error_feed(error_msg):
+def create_error_feed(error_msg, silent=False):
    """In case of an unrecoverable error print out an error feed and exit"""
-   from cgi import escape
-   error_id = "https://github.com/dgilman/atom_maker"
-   import datetime
-   now = rfc3339(datetime.datetime.utcnow())
-   print ('<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom"><title>Internal feed error</title><id>%s</id><updated>%s</updated><author><name>atom_maker</name></author><entry><title>%s</title><id>%s</id><updated>%s</updated><content type="html">%s</content></entry></feed>' % (error_id, now, error_msg.partition('\n')[0], error_id, now, escape("<pre>"+error_msg+"</pre>"))).encode('UTF-8')
    import sys
+   from cgi import escape
+   import datetime
+
+   if silent:
+      sys.exit()
+
+   now = rfc3339(datetime.datetime.utcnow())
+   guid = "https://github.com/dgilman/atom_maker" + "#" + now
+
+   rval = '<?xml version="1.0" encoding="utf-8"?><feed xmlns="http://www.w3.org/2005/Atom"><title>Internal feed error</title><id>%(id)s</id><updated>%(ts)s</updated><link rel="self" href="%(self)s"/><author><name>atom_maker</name></author><entry><title>%(title)s</title><id>%(id)s</id><updated>%(ts)s</updated><content type="html">%(content)s</content></entry></feed>' % ({"id": guid, "ts": now, "self": escape(self_url()), "title": error_msg.partition('\n')[0], "content": escape("<pre>"+error_msg+"</pre>")})
+   print rval.encode("UTF-8")
    sys.exit()
 
 def self_url():
    import os
    from cgi import escape
-   return "http://%s%s" % (os.environ['SERVER_NAME'].decode("UTF-8"), escape(os.environ['REQUEST_URI'].decode("UTF-8")))
+   return "http://%s%s" % (os.environ['SERVER_NAME'].decode("UTF-8"), os.environ['REQUEST_URI'].decode("UTF-8"))
 
 def warn_old(guid, id):
+   """guid: path to bug (foo.com/show_bug.cgi)
+      id: bug id"""
    import datetime
    now = rfc3339(datetime.datetime.utcnow())
    guid = guid + "#oldwarning_%d" % now.year # on january 1 feed readers will present a "new" reminder
